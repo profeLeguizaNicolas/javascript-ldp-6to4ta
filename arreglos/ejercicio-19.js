fa7df37d162beb7de1ac099056374c610c684dd6
cersosimovicente <128232790+cersosimovicente@users.noreply.github.com>
2023-05-12 20:58:18 -0300

    Update ejercicio-19.js

--- arreglos/ejercicio-19.js
+++ arreglos/ejercicio-19.js
@@ -1,1 +1,16 @@
+numberArray = [] //Declarra array vacio
+const getNumber = parseInt(prompt('Ingrese un número ')) //Declarar varoable y almcanar dato en ella
 
+// Crear bucle for para agregar un dato a la lista
+for(let index = 1; index <= getNumber; index++){
+  numberArray.push(index) //Agregar el dato index al array
+}
+
+// Crear bucle for para imprimir los datos de la lista
+for(let index = 0; index < numberArray.length; index++){
+  // Condicional if para validar y eliminar el dato con el indice correspondiente
+  if(index === 1){
+    numberArray.splice(index,index) //Elimina el dato del indice seleccionado
+  }
+  console.log(numberArray[index]) //Imprimir cada dato del array por medio del incide
+}
