a3363d9672717bc633ef90e1c2705cb1217516c9
cersosimovicente <128232790+cersosimovicente@users.noreply.github.com>
2023-05-12 15:56:16 -0300

    Create ejercicio-3.js

---
+++ basicos/ejercicio-3.js
@@ -0,0 +1,7 @@
+let num1 = prompt('Ingresa un numero: ')
+let num2 = prompt('Ingrese un segundo numero: ')
+let suma;
+
+suma = parseInt(num1) + parseInt(num2)
+
+console.log('La suma de los numeros es: ', suma)
