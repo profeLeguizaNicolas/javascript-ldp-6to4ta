a11a6e776b87cf0abfa64910170b63408d2f3737
cersosimovicente <128232790+cersosimovicente@users.noreply.github.com>
2023-05-12 20:09:08 -0300

    Update ejercicio-17.js

--- basicos/ejercicio-17.js
+++ basicos/ejercicio-17.js
@@ -1,1 +1,12 @@
-
+let number = "";
+let rnd = 0;
+while(rnd !== number){
+rnd = parseInt(Math.random() * 11);
+number = prompt("Adivina el numero: ");
+if(rnd == number) {
+  console.log(`Tu numero ${number} y el aleatorio     ${rnd}, Adivinaste!!`);
+  break;
+}
+else console.log(`Tu numero ${number} y el aleatorio ${rnd}, no acertaste, intenta de nuevo`);
+  
+}
