630d1f5db73915a3a7176a0721de9c48ee0acd49
cersosimovicente <128232790+cersosimovicente@users.noreply.github.com>
2023-05-12 20:59:27 -0300

    Update ejercicio-20.js

--- arreglos/ejercicio-20.js
+++ arreglos/ejercicio-20.js
@@ -1,1 +1,14 @@
+// Declarar array con datos que son arrys
+const mat = [ //array principal
+  ["Pablo", "Maria", "Pedro"], //array 1
+  ["Diana", "Juan", "Federico"], //array 2
+  ["Roberto", "Daniel", "Sandra"] //array 3
+]
 
+// Crear bucle forr anidados para poder acceder a los arrays dentro del array principal
+for(let indexA = 0; indexA < mat.length; indexA++){
+  console.log('Grupo ', indexA+1) // iMprime un anuncio
+  for(let indexB = 0; indexB < mat.length; indexB++){
+    console.log(' ',mat[indexA][indexB]) //Imprime dato de los arrays dentro del array princiapl
+  }
+}
