08a4a4ccf94e4181a9b196ee4a707a9cba5966e4
cersosimovicente <128232790+cersosimovicente@users.noreply.github.com>
2023-05-12 20:39:00 -0300

    Update ejercicio-18.js

--- arreglos/ejercicio-18.js
+++ arreglos/ejercicio-18.js
@@ -1,1 +1,11 @@
+let nombres = ["Pedro", "Pablo", "María", "Juan", "Diana"];
 
+const nombre1 = prompt("Ingrese un nombre");
+nombres.push(nombre1);
+
+const nombre2 = prompt("Ingrese otro nombre");
+nombres[2] = nombre2;
+
+for (let i = 0; i < nombres.length; i ++) {
+  console.log(nombres[i]);
+}
