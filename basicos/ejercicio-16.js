13d03328f851956e516f300936a00f749149e0f8
cersosimovicente <128232790+cersosimovicente@users.noreply.github.com>
2023-05-12 20:07:49 -0300

    Update ejercicio-16.js

--- basicos/ejercicio-16.js
+++ basicos/ejercicio-16.js
@@ -1,1 +1,6 @@
+const userNumber1=parseInt(prompt("Escriba el primer numero"))
+const userNumber2=parseInt(prompt("Escriba el segundo numero, mayor al primero"))
 
+for (let cont=userNumber1;cont<=userNumber2;cont++){
+  console.log(cont)
+}
