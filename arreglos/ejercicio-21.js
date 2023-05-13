77b5c7370e5a99090898bb1af4247cc6dd8859ad
cersosimovicente <128232790+cersosimovicente@users.noreply.github.com>
2023-05-12 21:00:29 -0300

    Update ejercicio-21.js

--- arreglos/ejercicio-21.js
+++ arreglos/ejercicio-21.js
@@ -1,1 +1,10 @@
+// Declarar varaible con datos
+const nums = [1, 23, 5, 8, 40, 12, 2, 67, 24, 9, 39] 
 
+// Crear ciclo for para imprimir cada valor del array por medio del index
+for(let index = 0; index < nums.length; index++){
+  // Condicional if para comparar si son mayores a 10 los valores del array
+  if(nums[index] > 10){ 
+    console.log(nums[index]) //Imprime valores del array 
+  }
+}
